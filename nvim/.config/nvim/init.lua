89e997e7d40e7098310db312b11b29d6bd9f7803
William Hleucka <william.hleucka@gmail.com>
2022-06-09 09:03:25 -0600

    Update nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,23 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then
+  impatient.enable_profile()
+end
 
-require('plugins')
-require('options')
-require('mappings')
+vim.opt.rtp:append(vim.fn.stdpath "config" .. "/../astronvim")
+
+for _, source in ipairs {
+  "core.utils",
+  "core.options",
+  "core.plugins",
+  "core.autocmds",
+  "core.mappings",
+  "core.ui",
+  "configs.which-key-register",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then
+    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+  end
+end
+
+astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
