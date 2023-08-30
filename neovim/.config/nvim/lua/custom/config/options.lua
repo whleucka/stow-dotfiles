fc9cde43cf9dc3a02ff0ee2728be1138ed8a1887
William Hleucka <william.hleucka@gmail.com>
2023-08-30 09:27:21 -0600

    fix: no more swaps

--- neovim/.config/nvim/lua/custom/config/options.lua
+++ neovim/.config/nvim/lua/custom/config/options.lua
@@ -23,6 +23,9 @@ vim.o.breakindent = true
 -- Save undo history
 vim.o.undofile = true
 
+-- No swap files
+vim.opt.swapfile = false
+
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
