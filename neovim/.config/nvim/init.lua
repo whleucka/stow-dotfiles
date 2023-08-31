f2b259e25a664ac0b257e0975822728ecb155200
William Hleucka <william.hleucka@gmail.com>
2023-08-31 09:24:31 -0600

    fix: moving stuff around, fix term

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,10 +2,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require "custom.config.options"
 require "custom.config.lazy"
 require "custom.config.telescope"
 require "custom.config.treesitter"
-require "custom.config.mappings"
+require "custom.options"
+require "custom.mappings"
 require "custom.config.autocmds"
 require "custom.config.lsp"
--- neovim/.config/nvim/lua/custom/mappings.lua
+++ neovim/.config/nvim/lua/custom/mappings.lua
@@ -5,6 +5,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Save
 vim.keymap.set('n', '<leader>w', ':update<CR>', { silent = true })
+vim.keymap.set('n', '<leader><space>', ':update<CR>', { silent = true })
 
 -- Quit
 vim.keymap.set('n', '<leader>q', ':q<CR>', { silent = true })
@@ -42,21 +43,23 @@ vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { de
 vim.keymap.set('n', '<leader>fq', require('telescope.builtin').quickfix, { desc = 'Find quickfix list' })
 
 -- Terminal
-vim.keymap.set('n', '<C-\\>', ':ToggleTerm dir=.<CR>', { silent = true })
+vim.keymap.set('n', '<C-\\>', ':ToggleTerm direction=float<CR>', { silent = true })
 vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>', { silent = true })
 vim.keymap.set('n', '<leader>th', ':ToggleTerm size=10 direction=horizontal<CR>', { silent = true })
 vim.keymap.set('n', '<leader>tv', ':ToggleTerm size=70 direction=vertical<CR>', { silent = true })
+
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'kj', [[<C-\><C-n>]], opts)
   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
   vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
-vim.cmd('autocmd! TermOpen term://toggleterm#* lua set_terminal_keymaps()')
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
 
 -- Splits
 vim.keymap.set('n', '|', '<C-w>v', { silent = true })
