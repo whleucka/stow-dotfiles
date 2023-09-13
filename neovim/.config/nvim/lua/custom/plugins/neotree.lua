a593bd7e0390dd9cf76100dbab17b72a91504ed9
William Hleucka <william.hleucka@gmail.com>
2023-09-13 07:27:18 -0600

    fix: neotree

--- neovim/.config/nvim/lua/custom/plugins/neotree.lua
+++ neovim/.config/nvim/lua/custom/plugins/neotree.lua
@@ -23,6 +23,32 @@ return {
           hide_hidden = false,
         },
       },
+      event_handlers = {
+        -- Auto Close on Open File
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end
+        },
+        {
+          event = "file_renamed",
+          handler = function(args)
+            -- fix references to file
+            vim.notify(args.source .. " renamed to " .. args.destination, vim.log.levels.INFO)
+          end
+        },
+        {
+          event = "file_moved",
+          handler = function(args)
+            -- fix references to file
+            vim.notify(args.source .. " moved to " .. args.destination, vim.log.levels.INFO)
+          end
+        },
+      }
     }
   end,
 }
