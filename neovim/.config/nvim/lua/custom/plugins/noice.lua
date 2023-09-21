3bd38c8f02479ddc0bbdf83921c68b89d4329296
William Hleucka <william.hleucka@gmail.com>
2023-09-20 19:56:40 -0600

    fix: update noice

--- neovim/.config/nvim/lua/custom/plugins/noice.lua
+++ neovim/.config/nvim/lua/custom/plugins/noice.lua
@@ -35,6 +35,7 @@ return {
       "rcarriga/nvim-notify",
       config = function()
         require('notify').setup({
+          background_colour = "#000000",
           top_down = false,
           timeout = 1000,
         })
