8361a3e1710c3bfde6da48b994df30c855ec96b1
William Hleucka <william.hleucka@gmail.com>
2023-09-20 20:04:31 -0600

    fix: trans background

--- neovim/.config/nvim/lua/custom/plugins/catppuccin.lua
+++ neovim/.config/nvim/lua/custom/plugins/catppuccin.lua
@@ -4,6 +4,9 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
+      integrations = {
+        neotree = true
+      },
       transparent_background = true
     })
     vim.cmd.colorscheme 'catppuccin-macchiato'
