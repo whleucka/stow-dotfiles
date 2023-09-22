cee9a88a443f493698177d85676d5fef67d0cc2a
William Hleucka <william.hleucka@gmail.com>
2023-09-22 10:47:22 -0600

    fix: diagnostics icons

--- neovim/.config/nvim/lua/custom/config/lsp.lua
+++ neovim/.config/nvim/lua/custom/config/lsp.lua
@@ -179,3 +179,16 @@ cmp.setup {
     { name = "rg",       dup = 0 },
   },
 }
+
+-- Diagnostics styles
+local signs = {
+    Error = " ",
+    Warning = " ",
+    Hint = " ",
+    Information = " "
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
