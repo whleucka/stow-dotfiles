2428fc87955160502f5bb57d2e04a5e4714e8785
William Hleucka <william.hleucka@gmail.com>
2023-09-26 12:06:06 -0600

    fix: toggle term border

--- neovim/.config/nvim/lua/custom/plugins/toggleterm.lua
+++ neovim/.config/nvim/lua/custom/plugins/toggleterm.lua
@@ -1,6 +1,16 @@
 return {
   'akinsho/toggleterm.nvim',
   version = "*",
+  opts = {
+    float_opts = {
+      border = "rounded",
+    },
+    highlights = {
+      FloatBorder = {
+        guifg = "#89b4fa",
+      },
+    }
+  },
   config = true
 }
 
