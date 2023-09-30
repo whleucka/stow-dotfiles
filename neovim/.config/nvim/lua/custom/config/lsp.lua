44fa4a5bb0fec4e65186d51b9e94dd1de5c5a478
William Hleucka <william.hleucka@gmail.com>
2023-09-30 14:42:50 -0600

    fix(telescope): find_files hidden true

--- neovim/.config/nvim/lua/custom/config/lsp.lua
+++ neovim/.config/nvim/lua/custom/config/lsp.lua
@@ -204,3 +204,11 @@ end
 
 -- Border on LSPInfo window
 require('lspconfig.ui.windows').default_options.border = 'rounded'
+
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
