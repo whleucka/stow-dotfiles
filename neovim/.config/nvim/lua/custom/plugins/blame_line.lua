81df3ceab7754830aef78d1cee1a28d63db8f261
William Hleucka <william.hleucka@gmail.com>
2023-10-03 09:40:26 -0600

    fix: add commit short to blame_line

--- neovim/.config/nvim/lua/custom/plugins/blame_line.lua
+++ neovim/.config/nvim/lua/custom/plugins/blame_line.lua
@@ -1,7 +1,9 @@
 return {
   "braxtons12/blame_line.nvim",
- 	config = function()
- 		require("blame_line").setup {}
+  config = function()
+    require("blame_line").setup {
+      template = "<commit-short> • <author> • <author-time> • <summary>",
+    }
     require("blame_line").disable()
- 	end
+  end
 }
