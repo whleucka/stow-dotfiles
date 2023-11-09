d4cbadd2128e6cc75968f332d5d23e931dd3df18
William Hleucka <william.hleucka@gmail.com>
2023-11-09 10:20:05 -0700

    fix: telescope

--- nvim/.config/nvim/lua/whleucka/plugins/telescope.lua
+++ nvim/.config/nvim/lua/whleucka/plugins/telescope.lua
@@ -3,7 +3,20 @@ return {
 	tag = '0.1.4',
 	cmd = "Telescope",
 	dependencies = { 'nvim-lua/plenary.nvim' },
-	opts = {},
+	opts = {
+		defaults = {
+			prompt_prefix = "🔎 ",
+			path_display = { "truncate" },
+			sorting_strategy = "ascending",
+			layout_config = {
+				horizontal = { prompt_position = "top", preview_width = 0.55 },
+				vertical = { mirror = false },
+				width = 0.87,
+				height = 0.80,
+				preview_cutoff = 120,
+			},
+		}
+	},
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
 		{ "<leader>fo", "<cmd>Telescope oldfiles<cr>",   desc = "Find old files" },
