53f8145df7fdfd542455368f41fb4bd922995fd0
William Hleucka <william.hleucka@gmail.com>
2023-11-10 08:12:14 -0700

    fix: keymaps for hapoon

--- nvim/.config/nvim/lua/whleucka/autocmd.lua
+++ nvim/.config/nvim/lua/whleucka/autocmd.lua
@@ -4,13 +4,13 @@ local grp = vim.api.nvim_create_augroup("GENERAL", { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
     group = grp,
     desc = 'Highlight after yank',
-    callback = function ()
-        vim.highlight.on_yank({higroup="IncSearch", timeout=120})
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 120 })
     end,
 })
 
 -- Restore last cursor position
-vim.api.nvim_create_autocmd({'BufWinEnter'}, {
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
     group = grp,
     desc = 'Return cursor to last position',
     pattern = '*',
@@ -23,3 +23,27 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     pattern = '*',
     command = [[%s/\s\+$//e]],
 })
+
+-- Start terminal in insert mode
+vim.api.nvim_create_autocmd('TermOpen', {
+    group   = grp,
+    pattern = '*',
+    command = 'startinsert | set winfixheight'
+})
+
+-- Exit help page pressing q
+vim.api.nvim_create_autocmd("FileType", {
+    group = grp,
+    pattern = {
+      "help",
+      "startuptime",
+      "qf",
+      "lspinfo",
+      "man",
+      "checkhealth",
+    },
+    command = [[
+            nnoremap <buffer><silent> q :close<CR>
+            set nobuflisted
+        ]],
+  })
--- nvim/.config/nvim/lua/whleucka/core/keymaps.lua
+++ nvim/.config/nvim/lua/whleucka/core/keymaps.lua
@@ -4,13 +4,44 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<leader>w', ':update<CR>', { silent = true, desc = "Save" })
 vim.keymap.set('n', '<leader>q', ':q<CR>', { silent = true, desc = "Close window" })
 
+-- Leave insert mode
+vim.keymap.set('i', 'jk', '<ESC>', { silent = true })
+vim.keymap.set('i', 'kj', '<ESC>', { silent = true })
+
 -- Buffers
 vim.keymap.set('n', '<leader>c', ':bd<CR>', { silent = true, desc = "Destroy buffer" })
-vim.keymap.set('n', 'H', ':bp<CR>', { silent = true, desc = "Previous buffer" })
-vim.keymap.set('n', 'L', ':bn<CR>', { silent = true, desc = "Next buffer" })
 
 -- Sourcing
 vim.keymap.set('n', '<c-s>', ':so<CR>', { silent = true, desc = "Source buffer" })
+
+-- Formatting
+vim.keymap.set('n', '<c-f>', function()
+	vim.lsp.buf.format { async = true }
+end, { silent = true, desc = "Format buffer" })
+
+-- Harpoon
+vim.keymap.set('n', '<leader>h', ':lua require("harpoon.mark").add_file()<cr>', { desc = 'Hapoon file' })
+vim.keymap.set('n', '<leader>hh', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', { desc = 'Hapoon quick menu' })
+vim.keymap.set('n', '<leader>1', ':lua require("harpoon.ui").nav_file(1)<cr>', { desc = 'Hapoon nav(1)' })
+vim.keymap.set('n', '<leader>2', ':lua require("harpoon.ui").nav_file(2)<cr>', { desc = 'Hapoon nav(2)' })
+vim.keymap.set('n', '<leader>3', ':lua require("harpoon.ui").nav_file(3)<cr>', { desc = 'Hapoon nav(3)' })
+vim.keymap.set('n', '<leader>4', ':lua require("harpoon.ui").nav_file(4)<cr>', { desc = 'Hapoon nav(4)' })
+vim.keymap.set('n', '<leader>5', ':lua require("harpoon.ui").nav_file(5)<cr>', { desc = 'Hapoon nav(5)' })
+vim.keymap.set('n', '<leader>6', ':lua require("harpoon.ui").nav_file(6)<cr>', { desc = 'Hapoon nav(6)' })
+vim.keymap.set('n', '<leader>7', ':lua require("harpoon.ui").nav_file(7)<cr>', { desc = 'Hapoon nav(7)' })
+vim.keymap.set('n', '<leader>8', ':lua require("harpoon.ui").nav_file(8)<cr>', { desc = 'Hapoon nav(8)' })
+vim.keymap.set('n', '<leader>9', ':lua require("harpoon.ui").nav_file(9)<cr>', { desc = 'Hapoon nav(9)' })
+vim.keymap.set('n', '<leader>0', ':lua require("harpoon.ui").nav_file(0)<cr>', { desc = 'Hapoon nav(0)' })
+vim.keymap.set('n', '<F9>', ':lua require("harpoon.term").gotoTerminal(1)<cr>', { desc = 'Hapoon terminal(1)' })
+vim.keymap.set('n', '<F10>', ':lua require("harpoon.term").gotoTerminal(2)<cr>', { desc = 'Hapoon terminal(2)' })
+vim.keymap.set('n', '<F11>', ':lua require("harpoon.term").gotoTerminal(3)<cr>', { desc = 'Hapoon terminal(3)' })
+vim.keymap.set('n', '<F12>', ':lua require("harpoon.term").gotoTerminal(4)<cr>', { desc = 'Hapoon terminal(4)' })
+vim.keymap.set('n', 'H', ':lua require("harpoon.ui").nav_prev()<cr>', { desc = 'Hapoon prev' })
+vim.keymap.set('n', 'L', ':lua require("harpoon.ui").nav_next()<cr>', { desc = 'Hapoon prev' })
+
+-- Splits
+vim.keymap.set('n', '|', '<C-w>v', { silent = true })
+vim.keymap.set('n', '\\', '<C-w>s', { silent = true })
 
 -- Lazy
 vim.keymap.set('n', '<leader>pu', ':Lazy update<CR>', { desc = "Lazy update", silent = true })
@@ -18,10 +49,6 @@ vim.keymap.set('n', '<leader>ps', ':Lazy sync<CR>', { desc = "Lazy sync", silent
 vim.keymap.set('n', '<leader>pc', ':Lazy clean<CR>', { desc = "Lazy clean", silent = true })
 vim.keymap.set('n', '<leader>pp', ':Lazy profile<CR>', { desc = "Lazy profile", silent = true })
 vim.keymap.set('n', '<leader>pd', ':Lazy debug<CR>', { desc = "Lazy debug", silent = true })
-
--- Splits
-vim.keymap.set('n', '|', '<C-w>v', { silent = true })
-vim.keymap.set('n', '\\', '<C-w>s', { silent = true })
 
 -- Quickfix
 vim.keymap.set('n', '[q', ':cp<CR>', { desc = 'Go to previous quickfix', silent = true })
@@ -31,29 +58,6 @@ vim.keymap.set('n', ']q', ':cn<CR>', { desc = 'Go to next quickfix', silent = tr
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message', silent = true })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message', silent = true })
 
--- Formatting
-vim.keymap.set('n', '<leader>f', function()
-	vim.lsp.buf.format { async = true }
-end, { silent = true, desc = "Format buffer" })
-
--- Leave insert mode
-vim.keymap.set('i', 'jk', '<ESC>', { silent = true })
-vim.keymap.set('i', 'kj', '<ESC>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- BOL/EOL
-vim.keymap.set('n', '<leader>h', '_', { silent = true, desc = "Jump to beginning of line" })
-vim.keymap.set('n', '<leader>l', '$', { silent = true, desc = "Jump to end of line" })
---vim.keymap.set('n', '<leader>j', 'G', { silent = true, desc = "Jump to bottom of file" })
---vim.keymap.set('n', '<leader>k', 'gg', { silent = true, desc = "Jump to top of file" })
-
--- Moving lines around
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
-
 -- Primeagen remaps
 vim.keymap.set("n", "J", "mzJ`z", { silent = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
@@ -61,3 +65,11 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
 vim.keymap.set("n", "n", "nzzzv", { silent = true })
 vim.keymap.set("n", "N", "Nzzzv", { silent = true })
 vim.keymap.set("n", "Q", "<nop>", { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Moving lines around
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
