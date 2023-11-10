d59fa1ac8c4c9ea88dbfc47d5da0168216890caf
William Hleucka <william.hleucka@gmail.com>
2023-11-10 07:24:13 -0700

    fix: autocmd file add

---
+++ nvim/.config/nvim/lua/whleucka/autocmd.lua
@@ -0,0 +1,25 @@
+local grp = vim.api.nvim_create_augroup("GENERAL", { clear = true })
+
+-- Highlight yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = grp,
+    desc = 'Highlight after yank',
+    callback = function ()
+        vim.highlight.on_yank({higroup="IncSearch", timeout=120})
+    end,
+})
+
+-- Restore last cursor position
+vim.api.nvim_create_autocmd({'BufWinEnter'}, {
+    group = grp,
+    desc = 'Return cursor to last position',
+    pattern = '*',
+    command = 'silent! normal! g`"zv',
+})
+
+-- Remove white space on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    group = grp,
+    pattern = '*',
+    command = [[%s/\s\+$//e]],
+})
