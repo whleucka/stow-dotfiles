413b44b1821784c7e7bcb79934710696a9913e19
William Hleucka <william.hleucka@gmail.com>
2023-11-15 08:36:57 -0700

    fix: lazy coke

--- nvim/.config/nvim/lua/whleucka/plugins/cokeline.lua
+++ nvim/.config/nvim/lua/whleucka/plugins/cokeline.lua
@@ -4,8 +4,8 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim", -- Required for v0.4.0+
 			"kyazdani42/nvim-web-devicons", -- If you want devicons
-			"stevearc/resession.nvim" -- Optional, for persistent history
 		},
-		config = true
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {},
 	}
 }
