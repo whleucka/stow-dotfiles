a2776472f1243a055f946b8caafe8406bdc6fbc7
William Hleucka <william.hleucka@gmail.com>
2023-11-16 08:50:16 -0700

    fix: auto resize windows/splits

--- nvim/.config/nvim/lua/whleucka/autocmd.lua
+++ nvim/.config/nvim/lua/whleucka/autocmd.lua
@@ -61,3 +61,16 @@ vim.api.nvim_create_autocmd("TermOpen", {
     pattern = [[term://*]],
     callback = set_terminal_keymaps
 })
+
+-- Resize windows/splits
+local wr_group = vim.api.nvim_create_augroup('WinResize', { clear = true })
+
+vim.api.nvim_create_autocmd(
+    'VimResized',
+    {
+        group = wr_group,
+        pattern = '*',
+        command = 'wincmd =',
+        desc = 'Automatically resize windows when the host window size changes.'
+    }
+)
