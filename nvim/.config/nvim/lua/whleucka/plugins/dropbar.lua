fc54c505b85c46186db6080b9d4aaf2703cf6729
William Hleucka <william.hleucka@gmail.com>
2024-02-21 07:32:58 -0700

    fix: add dropbar, only v10

---
+++ nvim/.config/nvim/lua/whleucka/plugins/dropbar.lua
@@ -0,0 +1,9 @@
+return {
+	-- Only works with nvim 0.10
+	--'Bekaboo/dropbar.nvim',
+	--event = { "BufReadPre", "BufNewFile" },
+    ---- optional, but required for fuzzy finder support
+    --dependencies = {
+    --  'nvim-telescope/telescope-fzf-native.nvim'
+    --}
+}
