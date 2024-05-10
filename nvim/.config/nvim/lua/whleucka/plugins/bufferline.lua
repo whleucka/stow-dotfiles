0d14604a64e09a23e443701956c27e6ec9ab2115
William Hleucka <william.hleucka@gmail.com>
2024-05-09 20:24:56 -0600

    fix: bufferline

--- nvim/.config/nvim/lua/whleucka/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/whleucka/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 return {
 	'akinsho/bufferline.nvim',
 	after = "catppuccin",
+	tag = "v4.5.2",
 	dependencies = 'nvim-tree/nvim-web-devicons',
 	config = function()
 		require("bufferline").setup {
