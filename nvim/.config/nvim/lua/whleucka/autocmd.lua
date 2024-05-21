c3211fde100b217e85329133cbec3e39ec10905a
William Hleucka <william.hleucka@gmail.com>
2024-05-20 20:16:28 -0600

    fix: update inlay hints

--- nvim/.config/nvim/lua/whleucka/autocmd.lua
+++ nvim/.config/nvim/lua/whleucka/autocmd.lua
@@ -67,3 +67,15 @@ vim.api.nvim_create_autocmd("TermOpen", {
     pattern = [[term://*]],
     callback = set_terminal
 })
+
+-- Inlay hints
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(args.buf, true)
+        end
+        -- whatever other lsp config you want
+    end
+})
