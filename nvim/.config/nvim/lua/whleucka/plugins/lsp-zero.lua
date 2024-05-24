97938dabe682bb2bedf95f113e24f36c1445b598
William Hleucka <william.hleucka@gmail.com>
2024-05-24 08:55:20 -0600

    fix: remove sign_define, will be removed in v12

--- nvim/.config/nvim/lua/whleucka/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/whleucka/plugins/lsp-zero.lua
@@ -272,19 +272,6 @@ return {
 			})
 
 			require('lspconfig.ui.windows').default_options.border = 'rounded'
-
-			-- Diagnostics styles
-			local signs = {
-				Error = "🤬",
-				Warn = "🔥",
-				Hint = "💡",
-				Info = "✅"
-			}
-
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
 		end
 	}
 }
