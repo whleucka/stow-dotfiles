f01019ef33076d5578c03799ac79e049a00b5412
William Hleucka <william.hleucka@gmail.com>
2024-07-02 17:41:15 -0600

    fix: remove spaces

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,10 +15,10 @@ require("whleucka.lazy")
 require("whleucka.autocmd")
 
 -- Theme
-vim.cmd.colorscheme "catppuccin"
+-- vim.cmd.colorscheme "catppuccin"
 --vim.cmd.colorscheme "nvimgelion"
--- vim.cmd("highlight Normal guibg=none")
--- vim.cmd("highlight NonText guibg=none")
--- vim.cmd("highlight LineNr guibg=none")
--- vim.cmd("highlight Folded guibg=none")
--- vim.cmd("highlight EndOfBuffer guibg=none")
+--vim.cmd("highlight Normal guibg=none")
+--vim.cmd("highlight NonText guibg=none")
+--vim.cmd("highlight LineNr guibg=none")
+--vim.cmd("highlight Folded guibg=none")
+--vim.cmd("highlight EndOfBuffer guibg=none")
