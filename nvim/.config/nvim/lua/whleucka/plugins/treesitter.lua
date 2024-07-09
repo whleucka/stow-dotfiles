a98293c6a0af08d83837cda32055f04c83d513a7
William Hleucka <william.hleucka@gmail.com>
2024-07-09 12:42:25 -0600

    fix: update treesitter move bindings

--- nvim/.config/nvim/lua/whleucka/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/whleucka/plugins/treesitter.lua
@@ -66,10 +66,10 @@ return {
 				swap = {
 					enable = true,
 					swap_next = {
-						['<c-]>'] = '@parameter.inner',
+						['<C-Right>'] = '@parameter.inner',
 					},
 					swap_previous = {
-						['<c-[>'] = '@parameter.inner',
+						['<C-Left>'] = '@parameter.inner',
 					},
 				},
 			},
