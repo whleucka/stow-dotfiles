b8376932f108d5afbaa2a0db203cfa4e27971ea9
William Hleucka <william.hleucka@gmail.com>
2024-07-13 08:47:35 -0600

    fix: update which-key v3

--- nvim/.config/nvim/lua/whleucka/plugins/which-key.lua
+++ nvim/.config/nvim/lua/whleucka/plugins/which-key.lua
@@ -1,39 +1,22 @@
 return {
 	"folke/which-key.nvim",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
 	event = "VeryLazy",
-	config = function()
-		local wk = require("which-key")
-		wk.register({
-			f = {
-				name = "File"
-			},
-			b = {
-				name = "Buffer"
-			},
-			d = {
-				name = "DAP"
-			},
-			g = {
-				name = "Neogit"
-			},
-			n = {
-				name = "Neotest"
-			},
-			x = {
-				name = "Trouble"
-			},
-			p = {
-				name = "Lazy (Package Manager)"
-			}
-		}, { prefix = "<leader>" })
-	end,
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	}
+		{ "<leader>b", group = "Buffer" },
+		{ "<leader>d", group = "DAP" },
+		{ "<leader>f", group = "File" },
+		{ "<leader>g", group = "Neogit" },
+		{ "<leader>n", group = "Neotest" },
+		{ "<leader>p", group = "Lazy (Package Manager)" },
+		{ "<leader>x", group = "Trouble" },
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
