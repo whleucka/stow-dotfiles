7b3609ce3a1e7abd9e5a6a57c920020ae67320f7
William Hleucka <william.hleucka@gmail.com>
2024-08-15 08:57:24 -0600

    wip: folds

--- nvim/.config/nvim/lua/whleucka/core/options.lua
+++ nvim/.config/nvim/lua/whleucka/core/options.lua
@@ -49,15 +49,15 @@ vim.g.python3_host_prog = "/usr/bin/python3"
 
 -- Folding
 -- Use Tree-sitter for folds
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
-vim.opt.foldlevelstart = 0   -- Close all folds by default
-vim.opt.foldminlines = 1     -- Minimum lines required for a fold to be created
-vim.opt.foldnestmax = 3      -- Maximum nested folds (adjust as needed)
+vim.o.foldlevelstart = 0   -- Close all folds by default
+vim.o.foldminlines = 1     -- Minimum lines required for a fold to be created
+vim.o.foldnestmax = 3      -- Maximum nested folds (adjust as needed)
 
 -- Optional: Keep fold text concise
-vim.opt.foldtext = [[substitute(getline(v:foldstart), '\t', repeat(' ', &tabstop), 'g') . '...' . trim(getline(v:foldend))]]
+vim.o.foldtext = [[substitute(getline(v:foldstart), '\t', repeat(' ', &tabstop), 'g') . '...' . trim(getline(v:foldend))]]
 
 -- Enable folding
-vim.opt.foldenable = true
+vim.o.foldenable = true
