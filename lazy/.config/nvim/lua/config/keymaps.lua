92153bae9d2085ddddcd858d7f404f1fa7b31906
William Hleucka <william.hleucka@gmail.com>
2024-08-24 22:08:12 -0600

    fix: update

--- lazy/.config/nvim/lua/config/keymaps.lua
+++ lazy/.config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local map = LazyVim.safe_keymap_set
+local map = vim.keymap.set
 
 -- Exit insert mode
 map("i", "jk", "<C-c>", { desc = "Exit insert mode" })
@@ -18,3 +18,6 @@ focusmap("h")
 focusmap("j")
 focusmap("k")
 focusmap("l")
+
+map("n", "<leader>ws", "<ESC>")
+map("n", "<leader>wv", "<ESC>")
