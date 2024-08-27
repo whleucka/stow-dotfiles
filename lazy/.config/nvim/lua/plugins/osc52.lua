7392a325ea08c4bd738d9862c14188e5cb81a04a
William Hleucka <william.hleucka@gmail.com>
2024-08-27 12:37:44 -0600

    fix: more yanks

--- lazy/.config/nvim/lua/plugins/osc52.lua
+++ lazy/.config/nvim/lua/plugins/osc52.lua
@@ -14,5 +14,9 @@ return {
     end
 
     vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+
+    vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true })
+    vim.keymap.set("n", "<leader>yy", "<leader>c_", { remap = true })
+    vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
   end,
 }
