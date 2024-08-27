bc0bf8a9ba77d35e04dae413944a3d06730431da
William Hleucka <william.hleucka@gmail.com>
2024-08-27 12:34:03 -0600

    fix: yank

--- lazy/.config/nvim/lua/plugins/osc52.lua
+++ lazy/.config/nvim/lua/plugins/osc52.lua
@@ -7,18 +7,12 @@ return {
       trim = false, -- Trim surrounding whitespaces before copy
       tmux_passthrough = false, -- Use tmux passthrough (requires tmux: set -g allow-passthrough on)
     })
-    local function copy(lines, _)
-      require("osc52").copy(table.concat(lines, "\n"))
+    function copy()
+      if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
+        require("osc52").copy_register("+")
+      end
     end
 
-    local function paste()
-      return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-    end
-
-    vim.g.clipboard = {
-      name = "osc52",
-      copy = { ["+"] = copy, ["*"] = copy },
-      paste = { ["+"] = paste, ["*"] = paste },
-    }
+    vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
   end,
 }
