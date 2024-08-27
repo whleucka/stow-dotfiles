52dd5b6815ed9fc6f762fd8f9d039b64d56a9934
William Hleucka <william.hleucka@gmail.com>
2024-08-27 12:56:27 -0600

    fix: update options

--- lazy/.config/nvim/lua/config/options.lua
+++ lazy/.config/nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local global = vim.g
 local opt = vim.opt
 
 opt.tabstop = 4
@@ -8,7 +9,7 @@ opt.expandtab = true
 opt.softtabstop = 4
 opt.shiftwidth = 4
 
-vim.g.clipboard = {
+global.clipboard = {
   name = "OSC 52",
   copy = {
     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
