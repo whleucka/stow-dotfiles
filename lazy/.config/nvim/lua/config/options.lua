12f46da6d75236d219d001427dd66555be0514a0
William Hleucka <william.hleucka@gmail.com>
2024-09-05 10:13:14 -0600

    fix: disable autoformat

--- lazy/.config/nvim/lua/config/options.lua
+++ lazy/.config/nvim/lua/config/options.lua
@@ -4,19 +4,21 @@
 local global = vim.g
 local opt = vim.opt
 
+global.autoformat = false
+
 opt.tabstop = 4
 opt.expandtab = true
 opt.softtabstop = 4
 opt.shiftwidth = 4
 
 global.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
 }
