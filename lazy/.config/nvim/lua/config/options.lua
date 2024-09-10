2f741c0bd3f09fc1bbbedc3896054fce0e415dad
William Hleucka <william.hleucka@gmail.com>
2024-09-10 07:31:37 -0600

    fix: update options

--- lazy/.config/nvim/lua/config/options.lua
+++ lazy/.config/nvim/lua/config/options.lua
@@ -10,21 +10,3 @@ opt.tabstop = 4
 opt.expandtab = true
 opt.softtabstop = 4
 opt.shiftwidth = 4
-
-function no_paste(reg)
-    return function(lines)
-        -- Do nothing! We can't paste with OSC52
-    end
-end
-
-global.clipboard = {
-    name = "OSC 52",
-    copy = {
-        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-        ["+"] = no_paste("+"), -- Pasting disabled
-        ["*"] = no_paste("*"), -- Pasting disabled
-    },
-}
