2e85aca2a8a0ffdf9740e952b6b4a732cc6bf74e
William Hleucka <william.hleucka@gmail.com>
2024-09-10 07:56:21 -0600

    fix: remove autocmd test

--- lazy/.config/nvim/lua/config/autocmds.lua
+++ lazy/.config/nvim/lua/config/autocmds.lua
@@ -11,14 +11,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         copy_to_unnamed(vim.v.event.regcontents)
     end
 })
-
-vim.api.nvim_create_autocmd({ 'ColorScheme', 'FileType' }, {
-  callback = function ()
-    vim.cmd([[
-      hi IndentBlanklineChar gui=nocombine guifg=#444C55
-      hi IndentBlanklineSpaceChar gui=nocombine guifg=#444C55
-      hi IndentBlanklineContextChar gui=nocombine guifg=#FB5E2A
-      hi IndentBlanklineContextStart gui=underline guisp=#FB5E2A
-    ]])
-  end,
-})
