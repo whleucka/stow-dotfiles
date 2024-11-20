bab2eac0ab9afe453e114820e9dc320533db8164
William Hleucka <william.hleucka@gmail.com>
2024-11-20 13:41:25 -0700

    fix: update plugs

--- lazy/.config/nvim/lua/config/keymaps.lua
+++ lazy/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+map("n", "q", "<nop>", { desc = "Nothing" })
 
 -- Exit insert mode
 map("i", "jk", "<C-c>", { desc = "Exit insert mode" })
@@ -25,3 +26,9 @@ map("n", "<leader>wv", "<ESC>")
 map("n", "<leader>w", "<cmd>update<cr>", { desc = "Update (save) buffer" })
 
 map("n", "<leader>gs", "<cmd>Neogit<cr>", { desc = "Neogit" })
+
+map("n", "<leader>tr", function() require("neotest").run.run() end, { desc = "Run the nearest test" })
+map("n", "<leader>ts", function() require("neotest").run.stop() end, { desc = "Stop the nearest test" })
+map("n", "<leader>ta", function() require("neotest").run.attach() end, { desc = "Attach the nearest test" })
+map("n", "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end, { desc = "Run the current file" })
+map("n", "<leader>tt", function() require("neotest").run.run({ suite = true }) end, { desc = "Run the test suite" })
