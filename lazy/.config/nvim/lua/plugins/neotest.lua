9705bda472d71542f4153a170a5016fce45d8b07
William Hleucka <william.hleucka@gmail.com>
2024-11-20 13:45:25 -0700

    fix: update neotest

---
+++ lazy/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-neotest/neotest",
+    lazy = true,
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "olimorris/neotest-phpunit",
+    },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-phpunit")
+            },
+        })
+    end
+}
