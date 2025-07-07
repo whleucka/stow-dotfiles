b1cbad2381ab5a80104991c0373ce8a4d6d5ec83
William Hleucka <william.hleucka@gmail.com>
2025-07-07 07:17:44 -0600

    fix: enhance theme

---
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -0,0 +1,7 @@
+-- Override highlights
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
