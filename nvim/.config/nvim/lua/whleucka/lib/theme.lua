9c6f80c828be8c092e5918e96d80d07d1716b87f
William Hleucka <william.hleucka@gmail.com>
2025-07-10 12:44:58 -0600

    fix: trans bg

--- nvim/.config/nvim/lua/whleucka/lib/theme.lua
+++ nvim/.config/nvim/lua/whleucka/lib/theme.lua
@@ -1,12 +1,10 @@
 vim.cmd [[colorscheme zaibatsu]]
 
--- Float bg
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000", blend = 0 })
-
 -- Transparent bg
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
--- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
--- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
--- vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
--- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
