66e4d5f668f3860e9bdea46f7c311e21fccb722a
William Hleucka <william.hleucka@gmail.com>
2025-07-10 14:34:28 -0600

    fix: add twig ftplug

---
+++ nvim/.config/nvim/ftplugin/twig.lua
@@ -0,0 +1,8 @@
+vim.bo.filetype = "html"
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.expandtab = true
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.bo.commentstring = "{# %s #}"
