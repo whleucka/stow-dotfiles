02ebbe981acaf355b83c32938b3065893d126274
William Hleucka <william.hleucka@gmail.com>
2025-07-24 21:45:30 -0600

    fix: add focus

--- nvim/.config/nvim/lua/plugins/focus-nvim.lua
+++ nvim/.config/nvim/lua/plugins/focus-nvim.lua
@@ -4,9 +4,11 @@ return {
     require("focus").setup()
   end,
   keys = function(map, opts)
-    map("n", "<C-l>", ":FocusSplitRight<CR>", opts)
-    map("n", "<C-h>", ":FocusSplitLeft<CR>", opts)
-    map("n", "<C-j>", ":FocusSplitDown<CR>", opts)
-    map("n", "<C-k>", ":FocusSplitUp<CR>", opts)
+    map("n", "<C-Right>", ":FocusSplitRight<CR>", opts)
+    map("n", "<C-Left>", ":FocusSplitLeft<CR>", opts)
+    map("n", "<C-Down>", ":FocusSplitDown<CR>", opts)
+    map("n", "<C-Up>", ":FocusSplitUp<CR>", opts)
+    map("n", "<F2>", ":FocusToggle<CR>", opts)
+    map("n", "<F3>", ":FocusAutoresize<CR>", opts)
   end
 }
