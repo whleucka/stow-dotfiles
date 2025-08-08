25241cb2f3218c966785866af9cb4d699f6dba51
William Hleucka <william.hleucka@gmail.com>
2025-08-08 07:39:40 -0600

    fix: add git commands

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -2,10 +2,10 @@ local explorer = require("lib.explorer")
 local opts = { noremap = true, silent = true }
 
 -- Essentials
+vim.keymap.set("n", "<F5>", ":update<CR> :source<CR>", opts)
 vim.keymap.set("n", "q", "<nop>", opts)
 vim.keymap.set("n", "<leader>q", ":quit<CR>", opts)
 vim.keymap.set("n", "<leader>w", ":write<CR>", opts)
-vim.keymap.set("n", "<leader>s", ":update<CR> :source<CR>", opts)
 vim.keymap.set("n", "<esc>", ":noh<CR>", opts)
 vim.keymap.set("i", "jk", "<esc>", opts)
 vim.keymap.set("i", "kj", "<esc>", opts)
@@ -14,9 +14,21 @@ vim.keymap.set("i", "kj", "<esc>", opts)
 vim.keymap.set("n", "<leader>o", ":Oil<CR>", opts)
 
 -- Git
-vim.keymap.set('n', '<leader>gs', ':Git<CR>', opts)
-vim.keymap.set('n', '<leader>gp', ':Git push<CR>', opts)
-vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', opts)
+vim.keymap.set('n', '<leader>gs', ':Git<CR>', opts) -- Git status
+vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit!<CR>', opts) -- Git diff in split
+vim.keymap.set('n', '<leader>gp', ':Git push<CR>', opts) -- Git push
+vim.keymap.set('n', '<leader>gf', ':Git fetch<CR>', opts) -- Git fetch
+vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', opts) -- Git pull
+vim.keymap.set('n', '<leader>gl', ':0Gclog<CR>', opts) -- Git log for current file
+vim.keymap.set("n", "<leader>ga", ":Git add %<CR>", opts) -- Stage current file
+vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", opts) -- Git blame current file
+vim.keymap.set("n", "<leader>gc", ":Git commit -v<CR>", opts) -- Git commit
+vim.keymap.set("n", "<leader>gco", ":Git checkout ", opts) -- Git checkout
+vim.keymap.set("n", "<leader>gm", ":Git merge ", opts) -- Git merge
+vim.keymap.set("n", "<leader>gr", ":Gdiffsplit!<CR>", opts) -- Resolve conflicts with :Gdiffsplit
+vim.keymap.set("n", "<leader>goc", ":diffget //2<CR>", opts) -- Take OUR changes
+vim.keymap.set("n", "<leader>gtc", ":diffget //3<CR>", opts) -- Take THIER changes
+vim.keymap.set("n", "<leader>gq", ":diffoff!<CR>", opts) -- Quit diff mode
 
 -- LSP
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)          -- Go to definition
@@ -39,6 +51,6 @@ vim.keymap.set("n", "<leader>\\", ":vsplit<CR>", opts)
 vim.keymap.set("n", "<leader>f", function()
   explorer.find_files()
 end, opts)
-vim.keymap.set("n", "<leader>g", function()
+vim.keymap.set("n", "<leader>s", function()
   explorer.grep_files()
 end, opts)
