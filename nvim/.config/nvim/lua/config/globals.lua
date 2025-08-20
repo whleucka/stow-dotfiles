af616c70ce53d8dae879003f2295da78efe92ab8
William Hleucka <william.hleucka@gmail.com>
2025-08-20 12:39:44 -0600

    fix: remove i3 easy nav

--- nvim/.config/nvim/lua/config/globals.lua
+++ nvim/.config/nvim/lua/config/globals.lua
@@ -1,8 +1,3 @@
--- i3-easy-nav
-vim.cmd[[call serverstart(tempname())]]
-vim.cmd[[let &titlestring="nvim %F -- [" . v:servername . "]"]]
-vim.cmd[[set title]]
-
 -- Leader, etc
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
