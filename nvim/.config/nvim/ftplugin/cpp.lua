1c68b7d31a03cb8c0e0fc36767392d80343ffe2e
William Hleucka <william.hleucka@gmail.com>
2025-09-02 14:36:47 -0600

    fix: update cpp ftplug

---
+++ nvim/.config/nvim/ftplugin/cpp.lua
@@ -0,0 +1,16 @@
+-- Set indentation width
+vim.opt_local.shiftwidth = 4   -- # of spaces for each indent level
+vim.opt_local.tabstop = 4      -- # of spaces a tab counts for
+vim.opt_local.softtabstop = 4  -- # of spaces when hitting Tab in insert mode
+vim.opt_local.expandtab = true -- Use spaces instead of tabs
+
+-- Optional: enable C-style auto-indenting
+vim.opt_local.cindent = true
+vim.opt_local.smartindent = true
+
+-- Optional: tweak `cindent` rules
+vim.opt_local.cinoptions = {
+  "g0", -- class scope: no extra indent
+  "N-s", -- namespace with no extra indent
+  "E-s", -- else at same level as if
+}
