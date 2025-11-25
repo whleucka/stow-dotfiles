989ea5a5d19051a611c97e4605a86c60d773c409
William Hleucka <william.hleucka@gmail.com>
2025-11-25 12:05:13 -0700

    fix: update lualine

--- nvim/.config/nvim/lua/plugins/lualine-nvim.lua
+++ nvim/.config/nvim/lua/plugins/lualine-nvim.lua
@@ -2,4 +2,63 @@ vim.pack.add {
   "https://github.com/nvim-lualine/lualine.nvim"
 }
 
-require('lualine').setup()
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+      refresh_time = 16, -- ~60fps
+      events = {
+        'WinEnter',
+        'BufEnter',
+        'BufWritePost',
+        'SessionLoadPost',
+        'FileChangedShellPost',
+        'VimResized',
+        'Filetype',
+        'CursorMoved',
+        'CursorMovedI',
+        'ModeChanged',
+      },
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'buffers'},
+    lualine_x = {},
+    lualine_y = {'encoding', 'fileformat'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {
+    'oil',
+    'fugitive',
+    'fzf',
+    'man',
+    'quickfix'
+  }
+}
