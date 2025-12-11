d64d6ccb2b8f286bb524816e92ae70220cd5ee67
William Hleucka <william.hleucka@gmail.com>
2025-12-11 14:42:48 -0700

    fix: update server url bodybuilder

--- nvim/.config/nvim/lua/plugins/bodybuilder-nvim.lua
+++ nvim/.config/nvim/lua/plugins/bodybuilder-nvim.lua
@@ -4,7 +4,9 @@ vim.pack.add({
   "https://github.com/whleucka/bodybuilder-nvim"
 })
 
+-- See https://github.com/ollama/ollama
 require("bodybuilder").setup({
-  model = "gemma3:270m",
+  model = "llama3.2",
+  url = "http://192.168.0.103:11434/api/generate",
   keymap = "<leader>af",
 })
