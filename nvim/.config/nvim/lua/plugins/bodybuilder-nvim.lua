11f16a53a76badbc52092bce13479ace065aa683
William Hleucka <william.hleucka@gmail.com>
2025-12-11 17:20:16 -0700

    fix: update plugs

--- nvim/.config/nvim/lua/plugins/bodybuilder-nvim.lua
+++ nvim/.config/nvim/lua/plugins/bodybuilder-nvim.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
   "https://github.com/nvim-lua/plenary.nvim",
   "https://github.com/nvim-treesitter/nvim-treesitter",
-  "https://github.com/whleucka/bodybuilder-nvim"
+  "https://github.com/whleucka/bodybuilder.nvim"
 })
 
 -- See https://github.com/ollama/ollama
